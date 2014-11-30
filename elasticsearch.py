86ab910a36a4be9873ba0f84eaea2b876732ebc1
sepich <i@sepa.spb.ru>
2014-11-30 17:46:33 +0300

    initial

---
+++ elasticsearch.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# Run without parameters to debug zabbix_sender
+# Run with param jvm.uptime_in_millis to trigger trap sending and return jvm.uptime_in_millis value
+# (active check used as trigger)
+sender='/usr/bin/zabbix_sender'      #path zabbix_sender
+cfg='/etc/zabbix/zabbix_agentd.conf' #path to zabbix-agent config
+tmp='/tmp/es_stats.tmp'              #temp file to use
+#list of traps to send
+traps1={                             #keys for health page
+  "status",
+  "active_primary_shards",
+  "active_shards",
+  "initializing_shards",
+  "relocating_shards",
+  "unassigned_shards"
+}
+traps2={                             #keys for cluster stats page
+  "indices.docs.count",
+  "indices.docs.deleted",
+  "indices.flush.total",
+  "indices.flush.total_time_in_millis",
+  "indices.get.exists_time_in_millis",
+  "indices.get.exists_total",
+  "indices.get.missing_time_in_millis",
+  "indices.get.missing_total",
+  "indices.indexing.delete_time_in_millis",
+  "indices.indexing.delete_total",
+  "indices.indexing.index_time_in_millis",
+  "indices.indexing.index_total",
+  "indices.merges.total",
+  "indices.merges.total_time_in_millis",
+  "indices.refresh.total",
+  "indices.refresh.total_time_in_millis",
+  "indices.search.fetch_time_in_millis",
+  "indices.search.fetch_total",
+  "indices.search.query_time_in_millis",
+  "indices.search.query_total",
+  "indices.store.throttle_time_in_millis",
+  "indices.warmer.total",
+  "indices.warmer.total_time_in_millis",
+  "jvm.mem.heap_committed_in_bytes",
+  "jvm.mem.heap_used_in_bytes",
+  "os.mem.actual_free_in_bytes",
+  "os.mem.actual_used_in_bytes"
+}
+
+import urllib
+import json
+import sys, os, time
+
+#read specified keys from json data
+def getKeys(stats,traps):
+  out=''
+  for t in traps:
+    c=t.split('.')
+    s=stats
+    while len(c):
+      s=s[c.pop(0)]
+    out += "- es.{0} {1}\n".format(t,s)
+  return out
+
+def main():
+  #load json data
+  try:
+    f = urllib.urlopen("http://localhost:9200/_cluster/health")
+    health = json.loads( f.read() )
+    f = urllib.urlopen("http://localhost:9200/_nodes/_local/stats?all=true")
+    all = json.loads( f.read() )
+    #only for current node
+    for node_id in all['nodes'].keys():
+      if all['nodes'][node_id]['hostname'] == os.uname()[1]:
+        all = all['nodes'][node_id]
+        if len(sys.argv)==1: print "node found"
+  except:
+    print "Unable to load Json data!"
+    sys.exit(1)
+
+  out = getKeys(health,traps1)  #getting health values
+  out += getKeys(all,traps2)    #getting stats  values
+
+  #write data for zabbix sender
+  if len(sys.argv)==1: print out
+  try:
+    with open(tmp,'w') as f: f.write(out)
+  except:
+    print "Unable to save data to send!"
+    sys.exit(1)
+
+  #return active check
+  if len(sys.argv)>1 and sys.argv[1]=='jvm.uptime_in_millis':
+    os.system("{0} -c {1} -i {2} >/dev/null 2>&1".format(sender,cfg,tmp))
+    print all['jvm']['uptime_in_millis']
+  #send data with debug
+  else:
+    os.system("{0} -c {1} -i {2} -vv".format(sender,cfg,tmp))
+
+  os.remove(tmp)
+
+if __name__ == "__main__":
+    main()
---
+++ rabbitmq.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# curl -ik -u guest:guest https://localhost:15672/api/overview
+# http://hg.rabbitmq.com/rabbitmq-management/raw-file/rabbitmq_v3_3_4/priv/www/api/index.html
+import json
+import sys, os
+import socket
+import urllib2
+
+username='guest'                     #rabbitmq user able to read data
+password='guest'                     #rabbitmq user's password$VAR{rabbitmq_zabbix_password}
+hostname = socket.gethostname()      #localhost or interface name
+port=15672                           #rabbitmq port
+sender='/usr/bin/zabbix_sender'      #path zabbix_sender
+cfg='/etc/zabbix/zabbix_agentd.conf' #path to zabbix-agent config
+tmp='/tmp/rb_stats.tmp'              #temp file to use
+
+#Call the REST API and convert the results into JSON
+def call_api(path):
+    url = 'https://{0}:{1}/api/{2}'.format(hostname, port, path)
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, url, username, password)
+    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+    return json.loads( urllib2.build_opener(handler).open(url).read() )
+
+def main():
+  #List all of the RabbitMQ queues
+  if len(sys.argv)<2:
+    print "Run as:\n{0} [list_queues | server check]".format(sys.argv[0])
+
+  elif sys.argv[1]=='list_queues':
+    if not socket.gethostname().endswith("-1"): return []  #only discover queues on first node
+    queues = []
+    for queue in call_api('queues'):
+      if not queue['name'].startswith("amq.gen-"):
+        element = {'{#VHOSTNAME}': queue['vhost'], '{#QUEUENAME}': queue['name']}
+        queues.append(element)
+    print json.dumps({'data': queues},indent=4)
+
+  elif sys.argv[1]=='server':
+    print call_api('nodes/rabbit@{0}'.format(hostname)).get(sys.argv[2])
+    #send all queues info as traps
+    if sys.argv[2]=='uptime':
+      out=''
+      for queue in call_api('queues'):
+        if queue['name'].startswith("amq.gen-"): continue
+        for item in ['messages', 'messages_unacknowledged', 'consumers']:
+          out += "- rabbitmq[{0},{1},{2}] {3}\n".format(queue['vhost'], queue['name'], item, queue.get(item, 0))
+
+      #write data for zabbix sender
+      try:
+        with open(tmp,'w') as f: f.write(out)
+      except:
+        print "Unable to save data to send!"
+        sys.exit(1)
+
+      #send data with debug
+      if len(sys.argv)>3 and sys.argv[3]=='debug':
+        print out
+        os.system("{0} -c {1} -i {2} -vv".format(sender, cfg, tmp))
+      else:
+        os.system("{0} -c {1} -i {2} >/dev/null 2>&1".format(sender, cfg, tmp))
+      #cleanup
+      os.remove(tmp)
+
+if __name__ == '__main__':
+    main()
